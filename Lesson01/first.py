691b5d1bf7722411520b992769f8a15f41faa704
uveshm003 <uveshmenpur.03@gmail.com>
2025-05-06 23:20:32 +0530

    Update first.py

---
+++ Lesson01/first.py
@@ -0,0 +1,11 @@
+# Python Comments
+# Print Statement 
+print("Hello My Friend",sep=' - ',end=' \n')
+
+# Storing User Inputs
+name = input("Please Enter Your Name:- ")
+
+# Multiple Operations on Single Line
+my_name, surname = "Uvesh" , "Menpurwala"
+
+print("Your Name is",name)
