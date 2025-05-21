39245fefec12aa36396704fd5e09a85d93d9abaa
uveshm003 <uveshmenpur.03@gmail.com>
2025-05-21 21:53:43 +0530

    Create practice1.py

---
+++ Lesson15/practice1.py
@@ -0,0 +1,20 @@
+def main():
+    print("Uvesh Menpurwala")
+    # Both are different Case Sensitive Language
+    item = "Banana"
+    Item = "Apple"
+    numeric = 19
+    abd = item + 19
+    print(abd)
+    print("Some Fruits are" + item + ", "+ Item)
+    for i in range(10):
+        print("Hello ")
+        
+
+main()
+
+def noLogicFunction():
+    pass
+
+def add(num, num2):
+    return num + num2
